422db41c10aa7fb5e3fd827dcb660db2839d13ac
ashpayne <ashpayne@gmail.com>
2020-12-07 20:28:22 +0000

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -217,25 +217,4 @@ Function Restart {
 	Restart-Computer
 }
 
-##########
-# Parse parameters and apply tweaks
-##########
 
-# Normalize path to preset file
-$preset = ""
-$PSCommandArgs = $args
-If ($args -And $args[0].ToLower() -eq "-preset") {
-	$preset = Resolve-Path $($args | Select-Object -Skip 1)
-	$PSCommandArgs = "-preset `"$preset`""
-}
-
-# Load function names from command line arguments or a preset file
-If ($args) {
-	$tweaks = $args
-	If ($preset) {
-		$tweaks = Get-Content $preset -ErrorAction Stop | ForEach { $_.Trim() } | Where { $_ -ne "" -and $_[0] -ne "#" }
-	}
-}
-
-# Call the desired tweak functions
-$tweaks | ForEach { Invoke-Expression $_ }
